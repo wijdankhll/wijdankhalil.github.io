6ab34cce50f46fc2b9fbe3ff23ee444e67d59016
wijdankhll <76167938+wijdankhll@users.noreply.github.com>
2025-06-22 15:05:31 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -66,3 +66,22 @@ document.addEventListener("DOMContentLoaded", function () {
   handleScroll();
 });
 
+let darkmode = localStorage.getItem('darkmode')
+const themeswitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active" ) enableDarkmode()
+
+themeswitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
